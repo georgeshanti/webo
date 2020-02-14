2dd37e9fd05f932ab02f3ccadcbe1f9efc370a28
George Thomas Shanti <georgeshanti@gmail.com>
2020-02-15 01:11:39 +0530

    first

---
+++ precache-manifest.cd7e54e755d276661868244efe1bba9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59384ba7b787cd5affe03d62f74af67b",
+    "url": "/webo/index.html"
+  },
+  {
+    "revision": "86521f510fa4c454f5c1",
+    "url": "/webo/static/css/main.43eb07e3.chunk.css"
+  },
+  {
+    "revision": "f5aeabb523dbc27f6c0c",
+    "url": "/webo/static/js/2.221716fc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/webo/static/js/2.221716fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86521f510fa4c454f5c1",
+    "url": "/webo/static/js/main.0f847e13.chunk.js"
+  },
+  {
+    "revision": "2a6517eb5f89b10d38cd",
+    "url": "/webo/static/js/runtime-main.ea93803d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webo/static/media/logo.5d5d9eef.svg"
+  }
+]);
